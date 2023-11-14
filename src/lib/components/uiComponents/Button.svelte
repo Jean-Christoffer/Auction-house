27371af86d7cc55d2ffc15b5d136c9139a071834
Jean Christoffer <christofferhalf6@gmail.com>
2023-11-14 16:53:27 +0100

    uiComponents (#17)

---
+++ src/lib/components/uiComponents/Button.svelte
@@ -0,0 +1,19 @@
+<button
+class="
+w-full
+bg-black
+hover:bg-gray-700
+text-white 
+text-center
+font-bold 
+py-2 px-4 
+text-l"
+>
+    {buttonText}
+</button>
+<script>
+    /**
+     * @type {string}
+    */
+   export let buttonText
+</script>
---
+++ src/lib/components/uiComponents/Cta.svelte
@@ -0,0 +1,40 @@
+<a 
+href={href}
+class="
+block
+w-full
+cursor-pointer
+border-solid 
+border-2
+font-bold  
+text-center
+
+{isFilled ? "hover:bg-gray-700" : "hover:bg-black"}
+{isFilled ? "hover:text-white" : "hover:text-white"}
+{isFilled ? "border-none" : "border-gray-700"}
+{isMobile ? "p-2" : "p-3"}
+{isFilled ? "bg-black" : ""}
+
+{isMobile ? "text-base" : "text-xl"}
+{isFilled ? "text-white" : ""}
+">{linkText}
+</a>
+
+<script>
+/**
+* @type {string}
+*/
+export let linkText
+/**
+* @type {string}
+*/
+export let href
+/**
+* @type {boolean}
+*/
+export let isMobile
+/**
+* @type {boolean}
+*/
+export let isFilled
+</script>
