9c534e7e732e6ff2533d31ae8d58d91d94948243
Jean Christoffer <89041011+Jean-Christoffer@users.noreply.github.com>
2023-11-14 16:51:58 +0100

    uiComponents

--- src/lib/components/uiComponents/SearchItem.svelte
+++ src/lib/components/uiComponents/SearchItem.svelte
@@ -1,3 +1,16 @@
-<div></div>
-<script></script>
+<figure class="max-w-sm rounded overflow-hidden shadow-lg">
+    <div>
+        <img class="w-full" alt=""/>
+    </div>
+    <article class="px-6 py-4">
+        <h3 class="font-bold text-xl mb-2">Item</h3>
+        <p class="text-gray-700 text-base">item details</p>
+    </article>
+    <div class="px-6 pt-4 pb-2">
+        <Cta linkText = "More details" href="#" isMobile={true} isFilled={false}/>
+    </div>
+</figure>
+<script>
+import Cta from "./Cta.svelte"
+</script>
 <style lang="postcss"></style>
